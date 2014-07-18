5ffb3f7f80dc01204906d1d4238d81fe11396524
Christopher Auyeung <chris@winfieldco.com>
2014-07-18 17:11:47 -0400

    updated to handle window resizes for responsive font sizing and text areas etc.

--- jquery.input_placeholder.js
+++ jquery.input_placeholder.js
@@ -5,10 +5,10 @@
 * Dual licensed under the MIT or GPL Version 2 licenses.
 */
 (function($){
-  
+
   $.fn.inputPlaceholder = function() {
     return this.each(function(){
-      $(this).find('input[data-placeholder]').each(function(){
+      $(this).find('input[data-placeholder], textarea[data-placeholder]').each(function(){
         var $input = $(this);
 
         var $container = $('<div><span>' + $input.attr('data-placeholder') + '</span></div>');
@@ -17,10 +17,13 @@
         var $span = $container.find('span');
 
         $span.css({
-          'position': 'absolute', 
+          'position': 'absolute',
           'left': $input.css('padding-left'),
           'top': $input.css('padding-top'),
           'font-size': $input.css('font-size'),
+          'font-weight': $input.css('font-weight'),
+          'font-style': $input.css('font-style'),
+          'font-size-adjust': $input.css('font-size-adjust'),
           'font-family': $input.css('font-family'),
           'user-select': 'none',
           '-moz-user-select': 'none',
@@ -40,8 +43,24 @@
           $input.focus();
         });
 
+        function onWindowResize() {
+          $container.find('span').css('font-size', $input.css('font-size'));
+        };
+
+        $(window).resize(function() {
+          onWindowResize();
+        });
+
+
+        $(document).ready(function() {
+          onWindowResize();
+        });
+
         var input_value = $input.val();
         setInterval(function(){
+
+
+
           if ($input.val() != input_value) {
             if ($input.val().length == 0) {
               $(this).val('');
@@ -55,5 +74,5 @@
       });
     });
   };
-  
+
 })(jQuery);
